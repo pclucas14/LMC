f34dd44641c84f783b2be26a2db0bace970a278f
Lucas Caccia <lucas.page-caccia@mail.mcgill.ca>
2021-08-31 13:37:03 -0700

    ckpt

--- eval.py
+++ eval.py
@@ -9,41 +9,17 @@ import numpy
 import numpy as np
 import argparse
 from model import VGG
+from data import CachedCIFAR10
 
-# --- Args
-parser = argparse.ArgumentParser()
+def get_args():
+    # --- Args
+    parser = argparse.ArgumentParser()
 
-""" optimization (fixed across all settings) """
-parser.add_argument('--path_a', type=str)
-parser.add_argument('--path_b', type=str)
-args = parser.parse_args()
-
-print(args)
-
-# Data Prep
-class CachedCIFAR10(torchvision.datasets.CIFAR10):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.data = torch.from_numpy(self.data).float().permute(0, 3, 1, 2)
-        self.targets = numpy.array(self.targets)
-
-        self.data = self.data / 255.
-
-        # normalize
-        mu  = torch.Tensor([0.4914, 0.4822, 0.4465]).view(1, 3, 1, 1)
-        var = torch.Tensor([0.2470, 0.2435, 0.2616]).view(1, 3, 1, 1)
-
-        self.data = (self.data - mu) / var
-
-
-    def __getitem__(self, index):
-        x, y = self.data[index], self.targets[index]
-
-        if self.transform is not None:
-            x = self.transform(x)
-
-        return x, y
+    """ optimization (fixed across all settings) """
+    parser.add_argument('--path_a', type=str)
+    parser.add_argument('--path_b', type=str)
+    args = parser.parse_args()
+    return args
 
 
 def model_interpolate(model_a, model_b, alpha):
@@ -63,7 +39,6 @@ def model_interpolate(model_a, model_b, alpha):
 @torch.no_grad()
 def eval(model, dl):
     model.eval()
-    #model.train()
 
     n_ok, n_total, loss = 0, 0, 0
     for (x,y) in dl:
@@ -78,72 +53,33 @@ def eval(model, dl):
     return n_ok / n_total, loss / n_total
 
 
-init_model = lambda : VGG(n_channels=32)
+if __name__ == '__main__':
+    args = get_args()
 
-test_dataset = CachedCIFAR10('../cl-pytorch/data', train=False, download=True)
-test_dl = torch.utils.data.DataLoader(test_dataset, batch_size=256, drop_last=False, shuffle=True)
+    init_model = lambda : VGG(n_channels=32)
 
-model_a = init_model()
-model_b = init_model()
+    test_dataset = CachedCIFAR10('../cl-pytorch/data', train=False, download=True)
+    test_dl = torch.utils.data.DataLoader(
+            test_dataset,
+            batch_size=256,
+            shuffle=True
+    )
 
-model_a.load_state_dict(torch.load(args.path_a))
-model_b.load_state_dict(torch.load(args.path_b))
+    model_a = init_model()
+    model_b = init_model()
 
-model_a.cuda(); model_b.cuda()
+    model_a.load_state_dict(torch.load(args.path_a))
+    model_b.load_state_dict(torch.load(args.path_b))
 
-acc, loss = eval(model_a, test_dl)
-print(f'Model A \t{acc:.4f}\t{loss:.4f}')
+    model_a.cuda(); model_b.cuda()
 
-acc, loss = eval(model_b, test_dl)
-print(f'Model B \t{acc:.4f}\t{loss:.4f}')
+    acc, loss = eval(model_a, test_dl)
+    print(f'Model A \t{acc:.4f}\t{loss:.4f}')
 
-for alpha in (np.arange(10) / 10.):
-    acc, loss = eval(model_interpolate(model_a, model_b, alpha=alpha), test_dl)
-    print(f'interpolating with alpha {alpha}. Accuracy : {acc:.4f}\tLoss : {loss:.4f}')
+    acc, loss = eval(model_b, test_dl)
+    print(f'Model B \t{acc:.4f}\t{loss:.4f}')
 
-
-'''
-PATH = '/private/home/lucaspc/repos/crlapi/crlapi/sl/weight_files/'
-
-# load models
-model_task_a   = init_model()
-model_task_a.load_state_dict(torch.load(PATH + 'VGG_n_channels:32_after0-2.pth'))
-
-model_task_a_again   = init_model()
-model_task_a_again.load_state_dict(torch.load(PATH + 'VGG_n_channels:32_after0-2-BOTH.pth'))
-
-
-model_task_b   = init_model()
-model_task_b.load_state_dict(torch.load(PATH + 'VGG_n_channels:32_after0-2-REV.pth'))
-
-model_task_iid  = init_model()
-model_task_iid.load_state_dict(torch.load(PATH + 'VGG_n_channels:32_after0-1.pth'))
-
-
-out = model_interpolate(model_task_a, model_task_iid, alpha=0.1)
-
-# do some interpolations
-model_task_a.cuda()
-acc, loss = eval(model_task_a, test_dl)
-print(f'Task A \t{acc:.4f}\t{loss:.4f}')
-
-model_task_b.cuda()
-acc, loss = eval(model_task_b, test_dl)
-print(f'Task B \t{acc:.4f}\t{loss:.4f}')
-
-model_task_iid.cuda()
-acc, loss = eval(model_task_iid, test_dl)
-print(f'IID \t{acc:.4f}\t{loss:.4f}')
-
-model_task_a_again.cuda()
-acc, loss = eval(model_task_a_again, test_dl)
-print(f'Task A again \t{acc:.4f}\t{loss:.4f}')
-
-acc, loss = eval(model_interpolate(model_task_b, model_task_a_again, alpha=0.5), test_dl)
-print(f'FROM A to A {acc:.4f}\t{loss:.4f}')
-
-acc, loss = eval(model_interpolate(model_task_a, model_task_iid, alpha=0.5), test_dl)
-print(f'interp 0.5 task a 0.5 task b {acc:.4f}\t{loss:.4f}')
-'''
-
+    for alpha in (np.arange(11) / 10.):
+        acc, loss = eval(model_interpolate(model_a, model_b, alpha=alpha), test_dl)
+        print(f'interpolating with alpha {alpha}. Accuracy : {acc:.4f}\tLoss : {loss:.4f}')
 
--- main.py
+++ main.py
@@ -5,55 +5,48 @@ import torchvision.datasets
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 
-import kornia
+import wandb
+import time
 import copy
 import numpy
+import kornia
 import argparse
 from model import VGG
+from data import CachedCIFAR10
+from eval import eval
 
 # --- Args
 parser = argparse.ArgumentParser()
 
 """ optimization (fixed across all settings) """
+parser.add_argument('--init_path', type=str, default='/checkpoint/lucaspc/LMC/cifar/ckpt/epoch_0.pth')
 parser.add_argument('--batch_size', type=int, default=128)
-parser.add_argument('--pickup_from', type=int, default=0)
 parser.add_argument('--save_prefix', type=str, default='')
-parser.add_argument('--save_every', type=int, default=5)
+parser.add_argument('--save_every', type=int, default=1)
+parser.add_argument('--replay', type=int, default=0)
+parser.add_argument('--n_tasks', type=int, default=1)
+parser.add_argument('--start_task', type=int, default=0)
+parser.add_argument('--end_task', type=int, default=-1)
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--task_order', type=int, nargs='+', default=None)
+parser.add_argument('--init_from_best', type=int, default=1)
+parser.add_argument('--delta', type=float, default=0.)
+parser.add_argument('--exp_name', type=str, default=None)
 
 args = parser.parse_args()
+if args.task_order is None:
+    args.task_order = numpy.arange(args.n_tasks)
+
 print(args)
 
-# Data Prep
-class CachedCIFAR10(torchvision.datasets.CIFAR10):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
-        self.data = torch.from_numpy(self.data).float().permute(0, 3, 1, 2)
-        self.targets = numpy.array(self.targets)
-
-        self.data = self.data / 255.
-
-        # normalize
-        mu  = torch.Tensor([0.4914, 0.4822, 0.4465]).view(1, 3, 1, 1)
-        var = torch.Tensor([0.2470, 0.2435, 0.2616]).view(1, 3, 1, 1)
-
-        self.data = (self.data - mu) / var
-
-
-    def __getitem__(self, index):
-        x, y = self.data[index], self.targets[index]
-
-        if self.transform is not None:
-            x = self.transform(x)
-
-        return x, y
+wandb.init(project='LMC', name=args.exp_name, config=args)
 
 init_model = lambda : VGG(n_channels=32)
 
 train_dataset = CachedCIFAR10('../cl-pytorch/data', train=True)
 test_dataset  = CachedCIFAR10('../cl-pytorch/data', train=False)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1024, shuffle=True)
+test_loader   = torch.utils.data.DataLoader(test_dataset, batch_size=1024)
 
 kn_augs = nn.Sequential(
     kornia.augmentation.RandomCrop(size=(32,32), padding=4, fill=-1.9),
@@ -61,51 +54,115 @@ kn_augs = nn.Sequential(
 ).cuda()
 
 model = init_model()
-
-# --- Load the appropriate checkpoint
-PATH = f'ckpt/epoch_{args.pickup_from}.pth'
-print(f'starting from {PATH}')
-model.load_state_dict(torch.load(PATH))
-
 model.cuda()
 
-print(model)
+# --- Load the appropriate checkpoint
+PATH = args.init_path
+print(f'starting from {PATH}')
+model.load_state_dict(torch.load(PATH))
 print(model[0][0].weight[0])
 
 opt = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
 
-best_model, best_acc = None, 0
 
-for epoch in range(args.pickup_from, 150):
-    # saving model here ensures `epochs` == number of epochs trained
-    if epoch > 0 and epoch % args.save_every == 0:
-        torch.save(model.state_dict(), f'ckpt/{args.save_prefix}epoch_{epoch}.pth')
+# --- Split Data into tasks
+# MAKE SURE ALWAYS SAME SPLIT
+total_size = len(train_dataset)
+chunks = [total_size // args.n_tasks] * args.n_tasks
+chunks = torch.utils.data.random_split(train_dataset, chunks, generator=torch.Generator().manual_seed(42))
+train_chunks, val_chunks = [], []
 
-    for (x,y) in train_loader:
-        x, y = x.cuda(), y.cuda()
-        x = kn_augs(x)
+for chunk in chunks:
+    size     = len(chunk)
+    tr_size  = int(size * 0.9)
+    val_size = size - tr_size
 
-        opt.zero_grad()
-        F.cross_entropy(model(x), y).backward()
-        opt.step()
-
-    if (epoch + 1) % 2 == 0:
-        n_ok, n_total = 0, 0
-
-        with torch.no_grad():
-            for (x,y) in test_loader:
-                x, y = x.cuda(), y.cuda()
-
-                n_ok += model(x).argmax(1).eq(y).sum()
-                n_total += x.size(0)
-
-            acc = n_ok / n_total
-
-            if acc > best_acc :
-                best_acc = acc
-                best_model = copy.deepcopy(model)
-
-            print(f'epoch {epoch} acc : {acc:.4f}')
+    tr_chunk, val_chunk = torch.utils.data.random_split(chunk, [tr_size, val_size], generator=torch.Generator().manual_seed(42))
+    train_chunks += [tr_chunk]
+    val_chunks   += [val_chunk]
 
 
-torch.save(best_model.state_dict(), f'final_model_augWS2.pth')
+
+# ---- LOOP over Mega-batches
+seen_tasks = []
+
+for task in args.task_order:
+
+    if len(seen_tasks) < args.start_task:
+        print(f'skipping task {task}')
+        seen_tasks += [task]
+        continue
+
+    seen_tasks += [task]
+
+    if args.replay:
+        task_train_ds = torch.utils.data.ConcatDataset([train_chunks[i] for i in seen_tasks])
+        task_val_ds   = torch.utils.data.ConcatDataset([val_chunks[i] for i in seen_tasks])
+    else:
+        task_train_ds = train_chunks[task]
+        task_val_ds   = val_chunks[task]
+
+    S = True
+    tr_loader  = torch.utils.data.DataLoader(task_train_ds, batch_size=args.batch_size, shuffle=S)
+    val_loader = torch.utils.data.DataLoader(task_val_ds, batch_size=1024, shuffle=S)
+
+    print(f'task {task}:\t{len(task_train_ds)} training & {len(task_val_ds)} val samples')
+
+    best_model  = model
+    best_acc, _ = eval(best_model, val_loader)
+
+    for epoch in range(args.epochs):
+
+        model.train()
+        start = time.time()
+        train_loss = 0
+
+        for it, (x,y) in enumerate(tr_loader):
+            x, y = x.cuda(), y.cuda()
+            x = kn_augs(x)
+
+            opt.zero_grad()
+            loss = F.cross_entropy(model(x), y)
+            loss.backward()
+            opt.step()
+
+            train_loss += loss.item()
+
+        epoch_time = time.time() - start
+
+        acc, loss = eval(model, val_loader)
+
+        if acc > (best_acc + args.delta):
+            best_acc = acc
+            best_model = copy.deepcopy(model)
+
+        wandb.log({
+            'task': task,
+            'train/loss':train_loss / (it+1),
+            'train/time': epoch_time,
+            'valid/acc': acc,
+            'valid/loss': loss}, step=args.epochs * task + epoch
+        )
+
+        print(f'epoch {epoch} acc : {acc:.4f}\t{epoch_time:.2f}')
+
+    if (task + 1) % args.save_every == 0:
+        print('saving')
+        torch.save(best_model.state_dict(), f'ckpt/{args.save_prefix}_best_t{task}.pth')
+        torch.save(model.state_dict(), f'ckpt/{args.save_prefix}_t{task}.pth')
+
+    if task == args.end_task and args.end_task > 0:
+        print(f'stopping after having seen {seen_tasks}')
+        exit()
+
+    # Start next task with best model
+    if args.init_from_best:
+        for pa, pb in zip(model.state_dict().values(), best_model.state_dict().values()):
+            pa.data.copy_(pb.data)
+
+    acc, loss = eval(model, test_loader)
+    print(f'test acc {acc:.4f}')
+    wandb.log({'test/acc': acc, 'test/loss': loss}, step=args.epochs * task + epoch)
+
+acc, loss = eval(model, test_loader)
+print(f'test acc {acc:.4f}')
